e3b282db0f6f0d2596cc283b6eb67bb561f58189
jonnystv <jonathan@darefoundation.com>
2021-01-06 16:12:13 +0000

    created first test, failed it; created room model

--- models/specs/room_spec.js
+++ models/specs/room_spec.js
@@ -1,4 +1,17 @@
 const assert = require('assert');
 const Room = require('../room.js');
 
-describe()
+describe('Room', function() {
+    let room;
+
+    beforeEach(function() {
+        room = new Room(20, false)
+    });
+
+    it("should have an area", function() {
+        const actual = room.area;
+        assert.strictEqual(actual, 20);
+
+    });
+
+});
